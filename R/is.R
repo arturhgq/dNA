2262d33b87d5b923e6f275f89d519469450cdafe
Artur Quirino <arturhgq@mail.com>
2022-10-14 09:25:15 -0300

    `not_na()` documentation

--- R/is.R
+++ R/is.R
@@ -1,4 +1,4 @@
-#' @title In x there are no missing elements?
+#' @title Is x a non-missing element?
 #' @description `r lifecycle::badge("stable")`
 #' This function indicates which elements are not missing
 #' @param x an \R object to be tested
